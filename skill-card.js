5fda1957b58a30aaf2828356808e970be5218efe
ChJoshna <91266334+ChJoshna@users.noreply.github.com>
2021-10-14 13:57:29 +0530

    Add files via upload

---
+++ skill-card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './skill-card.css';
+
+function Skillcard(skill) {
+    return (
+        <div className='skill-card'>
+         <div className='skill-icon'>
+         </div>   
+         <label className='skill-name'></label>
+        </div>
+    );
+}
+
+export default Skillcard;
